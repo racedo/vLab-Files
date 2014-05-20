3c09c7b02b4ef274a5700fd04706af9b75d4f9df
Ramon Acedo <ramon@linux-labs.net>
2014-05-20 15:24:46 +0100

    Updated with comments.

--- ovs-virt-install.sh
+++ ovs-virt-install.sh
@@ -1,12 +1,12 @@
 # If using directly a Open vSwitch bridge add this to the domain:
 #
-#     <interface type='bridge'>
-#        <source bridge='br-int0'/>
-#        <virtualport type='openvswitch' />
-#        <model type='virtio'/>
-#     </interface>
-# 
-# If using a Open vSwitch network previously defined with virsh add this to the domain (example with 3 networks):
+# <interface type='bridge'>
+#    <source bridge='br-int0'/>
+#    <virtualport type='openvswitch' />
+#    <model type='virtio'/>
+# </interface>
+#
+# If using a Open vSwitch network previously defined with virsh add this to the domain:
 #
 #    <interface type='network'>
 #      <source network='ovs-network' portgroup='no-vlan'/>
@@ -15,16 +15,23 @@
 #    <interface type='network'>
 #      <source network='ovs-network' portgroup='vlan-100'/>
 #      <model type='virtio'/>
-#    </interface>
+#   </interface>
 #    <interface type='network'>
 #      <source network='ovs-network' portgroup='vlan-200'/>
 #      <model type='virtio'/>
-#    </interface>
+#   </interface>
+#
+# Note that the order above makes eth0 in the no-vlan network, eth1 in the vlan-100 network and eth2 in the vlan-200 network.
+#
+# To boot from the network add this to the <os> section
+#
+#    <boot dev='network'/>
+#    <boot dev='hd'/>
 #
 # Create a pool to be able to use qcow2: virsh pool-define-as --name VMs-pool --type dir --target /home/VMs/pool/
 
 virt-install \
---name foreman \
+--name foreman-rhel \
 --ram 1024 \
 --vcpus=1 \
 --disk size=20,format=qcow2,pool=VMs-pool \
