f3a0dd4e30a04fbd0d4e6b3f4fff982e92e27dd3
Ramon Acedo <ramon@linux-labs.net>
2018-01-10 12:19:22 +0100

    Update delete-undercloud.sh

--- TripleO/delete-undercloud.sh
+++ TripleO/delete-undercloud.sh
@@ -3,4 +3,4 @@ yum autoremove openstack-* instack* mariadb* *heat* *director* mongodb* rabbitmq
 rm -rf \
 /var/log/nova /var/log/keystone /var/log/ironic* /var/log/mongodb /var/log/rabbitmq/ /var/log/heat /var/log/httpd /var/log/mistral /var/log/swift /var/log/zaqar /var/log/swift \
 /var/lib/rabbitmq/ /var/lib/glance /var/lib/haproxy /var/lib/heat* /var/lib/ironic* /var/lib/mongodb /var/lib/mysql /var/lib/neutron /var/lib/nova /var/lib/rabbitmq /var/lib/swift \
-/etc/glance /etc/haproxy /etc/heat /etc/httpd /etc/ironic-inspector /etc/mistral /etc/neutron /etc/nova /etc/rabbitmq /etc/swift /etc/tempest /etc/zaqar
+/etc/glance /etc/haproxy /etc/heat /etc/httpd /etc/ironic-inspector /etc/mistral /etc/neutron /etc/nova /etc/rabbitmq /etc/swift /etc/tempest /etc/zaqar /httpboot /tftpboot
