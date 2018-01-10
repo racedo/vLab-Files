974ccd9334e0823257bbb5dd2057900aa4e48288
Ramon Acedo <ramon@linux-labs.net>
2018-01-10 14:41:47 +0100

    Update delete-undercloud.sh

--- TripleO/delete-undercloud.sh
+++ TripleO/delete-undercloud.sh
@@ -1,6 +1,6 @@
-yum autoremove openstack-* instack* mariadb* *heat* *director* mongodb* rabbitmq-server httpd* python-swift haproxy
+yum autoremove openstack-* instack* mariadb* *heat* *director* mongodb* rabbitmq-server httpd* python-swift haproxy memcached docker*
 
 rm -rf \
 /var/log/nova /var/log/keystone /var/log/ironic* /var/log/mongodb /var/log/rabbitmq/ /var/log/heat /var/log/httpd /var/log/mistral /var/log/swift /var/log/zaqar /var/log/swift \
 /var/lib/rabbitmq/ /var/lib/glance /var/lib/haproxy /var/lib/heat* /var/lib/ironic* /var/lib/mongodb /var/lib/mysql /var/lib/neutron /var/lib/nova /var/lib/rabbitmq /var/lib/swift \
-/etc/glance /etc/haproxy /etc/heat /etc/httpd /etc/ironic-inspector /etc/mistral /etc/neutron /etc/nova /etc/rabbitmq /etc/swift /etc/tempest /etc/zaqar /httpboot /tftpboot /srv/node
+/etc/glance /etc/haproxy /etc/heat /etc/httpd /etc/ironic-inspector /etc/mistral /etc/neutron /etc/nova /etc/rabbitmq /etc/swift /etc/tempest /etc/zaqar /httpboot /tftpboot /srv/node /etc/xinetd.d/rsync
